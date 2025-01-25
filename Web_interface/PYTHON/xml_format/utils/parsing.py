5e5577e9513922f8867570024fee5169f22dca4c
FourbeFlo <floriane.goyus@gmail.com>
2025-01-25 10:25:01 +0100

    rm usless files

--- Web_interface/PYTHON/xml_format/utils/parsing.py
+++
@@ -1,6 +0,0 @@
-import xml.etree.ElementTree as ET
-
-def parse_xml(xml_content):
-    """Parse XML content into an ElementTree object and return the root."""
-    tree = ET.ElementTree(ET.fromstring(xml_content))
-    return tree.getroot()
