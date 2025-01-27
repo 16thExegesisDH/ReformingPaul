540227f6d496e8c3ae204723e7dcf7ffef389a2b
FourbeFlo <floriane.goyus@gmail.com>
2025-01-27 08:07:42 +0100

    update_path

--- Web_interface/JS/script.js
+++ Web_interface/JS/script.js
@@ -4,13 +4,15 @@ function openNav() {
 }
                     
 function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+  const sidebar = document.getElementById("mySidebar");
+  const main = document.getElementById("main");
+  sidebar.style.width = "0";
+  main.style.marginLeft = "0";
 }
 
 function toggleDocumentList() {
              const documentList = document.getElementById('documentList');
-             if (documentList.style.display === 'none') {
+             if  (documentList.style.display === 'none' || documentList.style.display === '') {
                  documentList.style.display = 'block';
              } else {
                  documentList.style.display = 'none';
